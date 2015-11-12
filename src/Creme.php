b9acc836be8e53dccf295227a0839d83a6392dee
Diego Brocanelli <diegod2@msn.com>
2015-11-12 19:50:33 -0200

    Correção de identação de comentários.

--- src/Creme.php
+++ src/Creme.php
@@ -3,36 +3,36 @@
 namespace Sorvete;
 
 /**
- * Classe para incremento de creme ao sorvete.
- *
- * @package Sorvete
- * @author Diego Brocanelli <diegod2@msn.com>
- */
+* Classe para incremento de creme ao sorvete.
+*
+* @package Sorvete
+* @author Diego Brocanelli <diegod2@msn.com>
+*/
 class Creme extends SorveteSabor
 {
 	/**
-	 * @param \Sorvete\Sorvete $sorvete.
-	 */
+	* @param \Sorvete\Sorvete $sorvete.
+	*/
 	public function __construct(Sorvete $sorvete)
 	{
 		parent::__construct($sorvete);
 	}
 
-    /**
-     * Retorna quantidade de bolas do sorvete.
-     *
-     * @return int
-	 */
+	/**
+	* Retorna quantidade de bolas do sorvete.
+	*
+	* @return int
+	*/
 	public function getQuantidadeBolas()
 	{
 		return 1 + $this->getSorvete()->getQuantidadeBolas();
 	}
 
-    /**
-     * Retorna o preço do sorvete.
-     *
-     * @return float
-     */
+	/**
+	* Retorna o preço do sorvete.
+	*
+	* @return float
+	*/
 	public function getQuantidadePreco()
 	{
 		return 1.25 + $this->getSorvete()->getQuantidadePreco();
